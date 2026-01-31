884e37b5b7a7f161b230d33b924daf93083b646b
Md Taiyeb Siyam <mdabdulauwaltaiyebsiyam@gmail.com>
2026-01-31 17:31:23 +0600

    Problem_2 completed

---
+++ problem_2.js
@@ -0,0 +1,15 @@
+//Subtask-1:
+
+for(let i = 61; i <= 100; i++){
+  if(i % 2 !== 0){
+    console.log("Odd number", i);
+  }
+}
+
+//Subtask-2:
+
+for(let i = 78; i <= 98; i++){
+    if(i % 2 === 0){
+    console.log("Even number", i);
+  }
+}
